955b5d2ef44123cccbc03f4b94dc8c7453a49e70
Vrishab Commuri <vrishabcommuri@vrishabs-mbp.wifi.local.cmu.edu>
2021-04-11 19:27:50 -0400

    improved server functionality

--- server.py
+++ server.py
@@ -4,13 +4,15 @@ import socket
 import numpy as np
 from concurrent.futures import ThreadPoolExecutor
 from scipy import signal
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
 
 UDP_IP = "172.26.7.230"
 UDP_PORT1 = 5005
 UDP_PORT2 = 5006
+SAMPLE_RATE = 2e6
 
-BUF_SIZE = int(10e6)
+BUF_SIZE = int(3e5)
 buffers = {UDP_PORT1:np.zeros(BUF_SIZE),
            UDP_PORT2:np.zeros(BUF_SIZE)}
 
@@ -22,7 +24,7 @@ def recv_udp_stream(port):
     
     # counter to track how much of the buffer we have filled
     c = 0
-    
+
     while True:
         data, addr = sock.recvfrom(1024)
 
@@ -33,25 +35,39 @@ def recv_udp_stream(port):
             port_buf[c] = arr[i]
             c += 1
         
-            if c == BUF_SIZE:
+            if c >= BUF_SIZE:
                 return
 
+def compute_buffer_delay():
+    buf0 = buffers[UDP_PORT1]
+    buf1 = buffers[UDP_PORT2]
+
+    xcorr = signal.correlate(buf0, buf1, 'valid')
+    delay = len(xcorr)/2 - np.argmax(np.abs(xcorr))
+
+    return delay
+
+# def delay_to_angle(delay):
+
+
+
 if __name__ == '__main__':
+    ############################################################################
+    # Compute delay for a given source
+    ############################################################################
 
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(recv_udp_stream, UDP_PORT1)
-        executor.submit(recv_udp_stream, UDP_PORT2)
+    while True:
+        input("Press 'Enter' to evaluate delay")
+        # spawn two threads -- one per antenna -- to collect data
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            executor.submit(recv_udp_stream, UDP_PORT1)
+            executor.submit(recv_udp_stream, UDP_PORT2)
 
-    print(len(buffers[UDP_PORT1]), len(buffers[UDP_PORT2]))
+        delay = compute_buffer_delay()
+        print(f"Repositioned source produced a delay of {delay} samples")
 
-    # normalize signals
-    data0 = buffers[UDP_PORT1]
-    data1 = buffers[UDP_PORT2]
+    
 
-    data0 = (data0 - np.mean(data0))/np.std(data0)
-    data1 = (data1 - np.mean(data1))/np.std(data1)
 
-    xcorr = signal.correlate(data0, data1)
+    
 
-    delay = len(xcorr)/2 - np.argmax(xcorr)
-    print(f"delay between elements is {delay} samples")
